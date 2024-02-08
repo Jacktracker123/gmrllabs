c4d132e0cdebb25694b0175b3d501a01d41fd4c9
raveena <raveenalaiju@gmail.com>
2024-02-09 00:46:31 +0530

    gmrllabs added

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from . models import *
+
+class Appointment_form(forms.ModelForm):
+    class Meta:
+        model=Appointment
+        fields='__all__'
+
+
+class Contact_form(forms.ModelForm):
+    class Meta:
+        model=Contact
+        fields='__all__'
